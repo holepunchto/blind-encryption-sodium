37ea6a2d21fdcc7e6298bbcf91f8081fa8be430b
Dominic Cassidy <dominic@cassidy.casa>
2026-01-19 19:01:51 +0200

    Support passing in new keys

--- index.js
+++ index.js
@@ -2,35 +2,57 @@ const b4a = require('b4a')
 const sodium = require('sodium-universal')
 
 class BlindEncryptionSodium {
-  constructor(entropy) {
-    this._entropy = entropy || null
-    this._type = 1
+  constructor(entropies) {
+    this._entropies = entropies.sort((a, b) => b.type - a.type)
 
-    this.encrypt = async (key) => {
-      const buffer = b4a.allocUnsafe(
-        key.byteLength + sodium.crypto_secretbox_MACBYTES + sodium.crypto_secretbox_NONCEBYTES
-      )
-      const nonce = buffer.subarray(0, sodium.crypto_secretbox_NONCEBYTES)
-      const box = buffer.subarray(nonce.byteLength)
+    this.encrypt = async (value) => {
+      // use latest
+      const entropy = this._entropies[0]
+      const buffer = this._encrypt(value, entropy.key)
 
-      sodium.randombytes_buf(nonce)
-      sodium.crypto_secretbox_easy(box, key, nonce, this._entropy)
-
-      return { value: buffer, type: 1 }
+      return { value: buffer, type: entropy.type }
     }
 
     this.decrypt = async ({ value, type }) => {
-      if (type !== this._type) {
-        throw new Error('Not encrypted using BlindEncryptionSodium')
+      const entropy = this._entropies[0]
+
+      // no backward compat
+      if (type > entropy.type) throw new Error('Encrypted using new type: ' + type)
+
+      // auto upgrade
+      if (type < entropy.type) {
+        const usedEntropy = this._entropies.find((e) => e.type === type)
+        if (!usedEntropy) throw new Error('Missing type: ' + type)
+
+        const decrypted = this._decrypt(value, usedEntropy.key)
+
+        return decrypted
       }
 
-      const nonce = value.subarray(0, sodium.crypto_secretbox_NONCEBYTES)
-      const box = value.subarray(nonce.byteLength)
-      const output = b4a.allocUnsafe(box.byteLength - sodium.crypto_secretbox_MACBYTES)
-
-      sodium.crypto_secretbox_open_easy(output, box, nonce, this._entropy)
-      return output
+      return this._decrypt(value, entropy.key)
     }
+  }
+
+  _encrypt(value, entropy) {
+    const buffer = b4a.allocUnsafe(
+      value.byteLength + sodium.crypto_secretbox_MACBYTES + sodium.crypto_secretbox_NONCEBYTES
+    )
+    const nonce = buffer.subarray(0, sodium.crypto_secretbox_NONCEBYTES)
+    const box = buffer.subarray(nonce.byteLength)
+
+    sodium.randombytes_buf(nonce)
+    sodium.crypto_secretbox_easy(box, value, nonce, entropy)
+
+    return buffer
+  }
+
+  _decrypt(value, entropy) {
+    const nonce = value.subarray(0, sodium.crypto_secretbox_NONCEBYTES)
+    const box = value.subarray(nonce.byteLength)
+    const output = b4a.allocUnsafe(box.byteLength - sodium.crypto_secretbox_MACBYTES)
+
+    sodium.crypto_secretbox_open_easy(output, box, nonce, entropy)
+    return output
   }
 }
 
--- test/index.js
+++ test/index.js
@@ -7,10 +7,61 @@ test('works', async (t) => {
   const encryptionKey = b4a.from('hello world', 'utf-8')
   const password = b4a.concat([b4a.from('my great password', 'utf-8')], 32)
 
-  const bes = new BlindEncryptionSodium(password)
+  const bes = new BlindEncryptionSodium([{ key: password, type: 0 }])
 
   const encryptedAndEncoded = await encrypt(encryptionKey, bes.encrypt)
   const decrypted = await decrypt(encryptedAndEncoded, bes.decrypt)
 
   t.is(decrypted.toString('utf-8'), 'hello world')
+})
+
+test('rotation', async (t) => {
+  const encryptionKey = b4a.from('hello world', 'utf-8')
+  const password = b4a.concat([b4a.from('my great password', 'utf-8')], 32)
+  const newPassword = b4a.concat([b4a.from('my greater password', 'utf-8')], 32)
+
+  let encryptedAndEncoded
+
+  {
+    const bes = new BlindEncryptionSodium([{ key: password, type: 0 }])
+
+    encryptedAndEncoded = await encrypt(encryptionKey, bes.encrypt)
+    const decrypted = await decrypt(encryptedAndEncoded, bes.decrypt)
+    t.is(decrypted.toString('utf-8'), 'hello world')
+  }
+
+  // method missing to rotate
+  t.exception(async () => {
+    const bes = new BlindEncryptionSodium([{ key: newPassword, type: 1 }])
+    await decrypt(encryptedAndEncoded, bes.decrypt)
+  }, /Missing type: 0/)
+
+  {
+    const bes = new BlindEncryptionSodium([
+      { key: password, type: 0 },
+      { key: newPassword, type: 1 }
+    ])
+
+    const decrypted = await decrypt(encryptedAndEncoded, bes.decrypt)
+    t.is(decrypted.toString('utf-8'), 'hello world')
+
+    // upgraded
+    encryptedAndEncoded = await encrypt(encryptionKey, bes.encrypt)
+  }
+
+  t.exception(async () => {
+    const bes = new BlindEncryptionSodium([{ key: password, type: 0 }])
+    const decrypted = await decrypt(encryptedAndEncoded, bes.decrypt)
+    t.is(decrypted.toString('utf-8'), 'hello world')
+  }, /Encrypted using new type: 1/)
+
+  // older version not needed use
+  {
+    const bes = new BlindEncryptionSodium([{ key: newPassword, type: 1 }])
+
+    const decrypted = await decrypt(encryptedAndEncoded, bes.decrypt)
+    t.is(decrypted.toString('utf-8'), 'hello world')
+
+    encryptedAndEncoded = await encrypt(encryptionKey, bes.encrypt)
+  }
 })
